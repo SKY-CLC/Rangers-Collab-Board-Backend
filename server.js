67c04e5a7a678ae5916c43fc6bcc451cadc0b26d
unknown <SKY-CLC>
2026-05-23 19:13:37 +0530

    User Model Created

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const app = require('./src/app')
+require('dotenv').config();
+const { connect } = require('mongoose');
+const app = require('./src/app');
+const connectDB = require('./src/db/db')
 
 
-
+connectDB();
 app.listen(3000,()=>{
    console.log("Server is running");
-})
+});
