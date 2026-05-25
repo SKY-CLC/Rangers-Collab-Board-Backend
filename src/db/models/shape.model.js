c3fd60062613e2535377c7e2a49d68081dd39230
Yuvraj Maurya <51598988-mauryayuvraj417@users.noreply.replit.com>
2026-05-25 13:02:52 +0000

    Create shapeModel

---
+++ src/db/models/shape.model.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const shapeSchema = new mongoose.Schema({
+  boardId: {
+    type: mongoose.Schema.Types.ObjectId,      ref: "board"
+  },
+  createdBy: {
+      type:mongoose.Schema.Types.ObjectId,
+      ref: "User"
+   },
+  type: {
+    type: String,
+    enum: [
+      "rectangle", 
+      "circle", 
+      "line", 
+      "arrow"
+    ]
+  },
+  position: {
+    x: Number,
+    y: Number
+  },
+
+  width: Number,
+  height: Number,
+
+  radius: Number,
+
+  startX: Number,
+  startY: Number,
+
+  endX: Number,
+  endY: Number,
+
+  color: String,
+  
+},{ timestamps: true });
+
+const shapeModel = mongoose.model("shape",shapeSchema);
+
+module.exports = shapeModel;
